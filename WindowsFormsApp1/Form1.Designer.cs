0a966d932a34e380a671f61874683d3f76f4ff19
Fejcvk <tomaszfejcak@icloud.com>
2018-03-02 00:51:42 +0100

    linear filters done

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,162 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(102, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(703, 574);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1002, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(703, 574);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(102, 621);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(108, 41);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "inverse";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(251, 621);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(222, 41);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "brightness correction";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(945, 621);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(192, 41);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Apply convolution filter";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Blur",
+            "Gaussisan smoothing",
+            "Sharpener",
+            "Edge detectection",
+            "Emobss"});
+            this.comboBox1.Location = new System.Drawing.Point(701, 628);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(216, 28);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(364, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Original image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1311, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Processed image";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(520, 621);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(108, 41);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "contrast";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(1159, 621);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(108, 41);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Clear";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1852, 926);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -15,6 +15,111 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = WindowsFormsApp1.Properties.Resources.falcon2;
+            pictureBox2.Image = WindowsFormsApp1.Properties.Resources.falcon2;
+        }
+        #region inverse filter
+        private void button6_Click(object sender, EventArgs e)
+        {
+            inversionFilter((Bitmap)pictureBox1.Image);
+        }
+        private void inversionFilter(Bitmap bmp)
+        {
+            for(int y = 0; y < bmp.Height; y++)
+            {
+                for(int x = 0; x < bmp.Width; x++)
+                {
+                    var pixel = bmp.GetPixel(x, y);
+                    var r = 255 - pixel.R;
+                    var g = 255 - pixel.G;
+                    var b = 255 - pixel.B;
+                    bmp.SetPixel(x, y, Color.FromArgb(pixel.A, r, g, b));
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+        #endregion
+        //clear image
+        private void button10_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = WindowsFormsApp1.Properties.Resources.falcon2;
+        }
+        #region brighntess filter
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var brightnessCoefficient = -30;
+            brightnessConvertion((Bitmap)pictureBox1.Image, brightnessCoefficient);
+        }
+
+        private void brightnessConvertion(Bitmap bmp, int brightnessCoefficient)
+        {
+            for(int y = 0; y < bmp.Height; y++)
+            {
+                for(int x = 0; x < bmp.Width; x++)
+                {
+                    var pixel = bmp.GetPixel(x, y);
+
+                    var r = 0;
+                    if (pixel.R + brightnessCoefficient < 0) r = 0;
+                    else r = (pixel.R + brightnessCoefficient < 255) ? pixel.R + brightnessCoefficient : 255;
+
+                    var g = 0;
+                    if (pixel.G + brightnessCoefficient < 0) g = 0;
+                    else g = (pixel.G + brightnessCoefficient < 255) ? pixel.G + brightnessCoefficient : 255;
+
+                    var b = 0;
+                    if (pixel.B + brightnessCoefficient < 0) b = 0;
+                    else b = (pixel.B + brightnessCoefficient < 255) ? pixel.B + brightnessCoefficient : 255;
+
+                    bmp.SetPixel(x, y, Color.FromArgb(pixel.A, r, g, b));
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+        #endregion
+        #region contrast filter
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var contrastCoefficient = 2.0 ;
+            contrastFilter((Bitmap)pictureBox2.Image, contrastCoefficient);
+        }
+        private void contrastFilter(Bitmap bmp, double contrastCoefficient)
+        {
+            for(int y = 0; y < bmp.Height; y++)
+            {
+                for(int x = 0; x < bmp.Width; x++)
+                {
+                    var pixel = bmp.GetPixel(x, y);
+                    var r = 0;
+                    if (contrastEnchancement((double)pixel.R, contrastCoefficient) < 0) r = 0;
+                    else r = (contrastEnchancement((double)pixel.R, contrastCoefficient) < 255.0) ? (int)contrastEnchancement((double)pixel.R, contrastCoefficient) : 255;
+
+                    var g = 0;
+                    if (contrastEnchancement((double)pixel.G, contrastCoefficient) < 0) g = 0;
+                    else g = (contrastEnchancement((double)pixel.G, contrastCoefficient) < 255.0) ? (int)contrastEnchancement((double)pixel.G, contrastCoefficient) : 255;
+
+                    var b = 0;
+                    if (contrastEnchancement((double)pixel.B, contrastCoefficient) < 0) b = 0;
+                    else b = (contrastEnchancement((double)pixel.B, contrastCoefficient) < 255.0) ? (int)contrastEnchancement((double)pixel.B, contrastCoefficient) : 255;
+
+                    bmp.SetPixel(x, y, Color.FromArgb(pixel.A, r, g, b));
+                }
+            }
+            pictureBox2.Image = bmp;
+        }
+        private double contrastEnchancement(double channelValue, double contrastCoefficient)
+        {
+            return ((((channelValue / 255.0) - 0.5) * contrastCoefficient) + 0.5) * 255.0;
+        }
+        #endregion
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+           // convolutionFilter( matrix, offset);
+        }
+        private void convolutionFilter(int [,] matrix, int offset)
+        {
+
         }
     }
 }
